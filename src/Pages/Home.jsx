b677dce823d5bd26aafdd60bbe831a6170175f03
arnav <arnavmandlik1@gmail.com>
2025-11-29 13:37:29 +0530

    Update profile image source in Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -28,7 +28,7 @@ export default function Home() {
         {/* Profile Image */}
         <div className="bg-[#141414] rounded-full overflow-hidden mb-6 w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] border-4 border-gray-700">
           <img
-            src="https://media.licdn.com/dms/image/v2/D5603AQGTU3P0sY69Ug/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722358398772?e=1766016000&v=beta&t=jEJvzSynapYKd5LnP4g9RIlBMiTlYj_yHTBUv_tfh3g"
+            src="https://storage.googleapis.com/creatorspace-public/users%2Fclmza562300dwq301fv2kegam%2FNjN4W9FBvqBbwmL7-WhatsApp%2520Image%25202024-07-30%2520at%252010.01.35%2520PM.jpg"
             alt="Profile"
             className="w-full h-full object-cover"
           />
